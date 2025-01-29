0bfc6f005ab281ea7056f4188b644a5d511213b2
Windows <jonathannithi@gmail.com>
2025-01-29 11:50:16 +0530

    Completed server.go under catalog

--- backend/catalog/server.go
+++ backend/catalog/server.go
@@ -1,7 +1,9 @@
 package catalog
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"net"
 
 	"github.com/JonathanNithi/ecommerce/backend/account/pb"
@@ -27,14 +29,62 @@ func ListenGRPC(s Service, port int) error {
 	return serv.Serve(lis)
 }
 
-func (s *grpc) PostProduct {
-
+func (s *grpcServer) PostProduct(ctx context.Context, r *pb.PostProductRequest) (*pb.PostProductResponse, error) {
+	p, err := s.service.PostProduct(ctx, r.Name, r.Description, r.Price)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+	return &pb.PostProductResponse{Product: &pb.Product{
+		Id:          p.ID,
+		Name:        p.Name,
+		Description: p.Description,
+		Price:       p.Price,
+	}}, nil
 }
 
-func (s *grpc) GetProduct {
-
+func (s *grpcServer) GetProduct(ctx context.Context, r *pb.GetProductRequest) (*pb.GetProductResponse, error) {
+	p, err := s.service.GetProduct(ctx, r.Id)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+	return &pb.GetProductResponse{
+		Product: &pb.Product{
+			Id:          p.ID,
+			Name:        p.Name,
+			Description: p.Description,
+			Price:       p.Price,
+		},
+	}, nil
 }
 
-func (s *grpc) GetProducts {
+func (s *grpcServer) GetProducts(ctx context.Context, r *pb.GetProductsRequest) (*pb.GetProductsResponse, error) {
+	var res []Product
+	var err error
+	if r.Query != "" {
+		res, err = s.service.SearchProducts(ctx, r.Query, r.Skip, r.Take)
+	} else if len(r.Ids) != 0 {
+		res, err = s.service.GetProductsByIDs(ctx, r.Ids)
+	} else {
+		res, err = s.service.GetProducts(ctx, r.Skip, r.Take)
+	}
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
 
+	products := []*pb.Product{}
+	for _, p := range res {
+		products = append(
+			products,
+			&pb.Product{
+				Id:          p.ID,
+				Name:        p.Name,
+				Description: p.Description,
+				Price:       p.Price,
+			},
+		)
+	}
+	return &pb.GetProductsResponse{Products: products}, nil
 }
