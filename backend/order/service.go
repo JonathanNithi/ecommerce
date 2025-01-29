431fce818a34a5502ea8583a0937369b29350a42
Windows <jonathannithi@gmail.com>
2025-01-29 16:52:25 +0530

    Completed repository.go under order folder and started on server.go and service.go

---
+++ backend/order/service.go
@@ -0,0 +1,19 @@
+package order
+
+import (
+	"context"
+	"time"
+)
+
+type Service interface {
+	PostOrder(ctx context.Context, accountID string, products []OrderedProduct) (*Order, error)
+	GetOrdersForAccount(ctx context.Context, accountID string) ([]Order, error)
+}
+
+type Order struct {
+	ID         string
+	CreatedAt  time.Time
+	TotalPrice float64
+	AccountID  string
+	Products   []OrderedProduct
+}
