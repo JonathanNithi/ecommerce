ab1eed7a6af9cc657d855ff2d8977c35025241f5
Windows <jonathannithi@gmail.com>
2025-02-10 19:16:10 +0530

    Modified up.sql under account folder

--- backend/account/up.sql
+++ backend/account/up.sql
@@ -1,8 +1,10 @@
+CREATE TYPE user_role AS ENUM ('admin', 'user');
+
 CREATE TABLE IF NOT EXISTS accounts (
   id CHAR(27) PRIMARY KEY,
   first_name VARCHAR(255) NOT NULL, 
   last_name VARCHAR(255) NOT NULL, 
   email VARCHAR(255) NOT NULL UNIQUE, 
   password_hash VARCHAR(255) NOT NULL, 
-  role ENUM('admin', 'user') NOT NULL
+  role user_role NOT NULL 
 );
