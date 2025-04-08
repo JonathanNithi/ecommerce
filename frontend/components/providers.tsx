837533adde3bdda3885b12c325ee9d63827bd182
Windows <jonathannithi@gmail.com>
2025-04-08 19:29:51 +0530

    Included authprovider in the providers file

--- frontend/components/providers.tsx
+++ frontend/components/providers.tsx
@@ -3,11 +3,12 @@
 import type React from "react"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
 import { CartProvider } from "@/context/cart-context"
+import { AuthProvider } from "@/lib/auth-context"
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <NextThemesProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
-      <CartProvider>{children}</CartProvider>
+      <AuthProvider ><CartProvider>{children}</CartProvider></AuthProvider>
     </NextThemesProvider>
   )
 }
