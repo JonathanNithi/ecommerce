5a61638502afe016806e137db7f01906a3c5d6b8
Windows <jonathannithi@gmail.com>
2025-04-10 01:10:40 +0530

    Added query

--- frontend/graphql/queries/product-queries.ts
+++ frontend/graphql/queries/product-queries.ts
@@ -48,6 +48,14 @@ export const SEARCH_PRODUCTS = gql`
   }
 `;
 
+export const GET_PRODUCTS_BY_IDS = gql`
+query GetProductsByIds($id: [String!]!) {
+  productsById(id: $id) {
+    id
+    availability
+  }
+}
+`;
 // Define the enum types if they are not already defined elsewhere in your codebase
 // These should match your GraphQL schema definitions
 export enum ProductSortField {
