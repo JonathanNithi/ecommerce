d5d363998a3910c444cfbfeb00ac29c082b63823
Windows <jonathannithi@gmail.com>
2025-04-09 16:16:57 +0530

    Updated homepage

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,26 +1,12 @@
-"use client";
+// app/page.tsx ("use client" is already implied for components in the app router)
 
 import Navbar from "@/components/navbar/Navbar";
 import Footer from "@/components/footer/Footer";
-import { ApolloProvider } from "@apollo/client";
-import { useMemo } from "react";
-import { createApolloClient } from "@/lib/create-apollo-client";
 
 export default function Home() {
-  const client = useMemo(() => createApolloClient(), []);
-
-  return (
-    <ApolloProvider client={client}>
-      <HomePageContent />
-    </ApolloProvider>
-  );
-}
-
-function HomePageContent() {
   return (
     <div className="flex min-h-screen flex-col">
       <Navbar />
-
       <main className="flex-1 pt-16 md:pt-16 ">
         <section className="relative bg-[url('/images/homepage.jpeg')] bg-cover bg-center h-screen flex items-center justify-center">
           <div className="absolute inset-0 bg-black/40" />
